da594f2bd3fee7902b98ba1e3b7837d9b20c408b
Enrique2442 <107508345+TeresaRuiz@users.noreply.github.com>
2024-07-09 09:29:02 -0600

    Comentando codigo

--- src/components/Productos/SelectProduct.js
+++ src/components/Productos/SelectProduct.js
@@ -2,29 +2,29 @@ import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, ScrollView 
 import { useState } from 'react';
 
 
-export default function SelectProduct({ip, imagenProducto, idProducto, nombreProducto, descripcionProducto
-    , precioProducto, existenciasProducto, accionBotonProducto
-}){
-
-    return(
-
-        <View style={styles.card}>
-        <View style={styles.imageContainer}>
-         <Image
-           source={{uri: `${ip}/NewPowerLetters/api/images/productos/${imagenProducto}`}}
-           style={styles.image}
-           resizeMode="contain" // Ajustar la imagen al contenedor
-         />
-       </View>
-       <Text style={styles.text}>{idProducto}</Text>
-       <Text style={styles.textTitle}>{nombreProducto}</Text>
-       <Text style={styles.text}>{descripcionProducto}</Text>
-       <Text style={styles.textTitle}>Precio: <Text style={styles.textDentro}>${precioProducto}</Text></Text>
-       <Text style={styles.textTitle}>Existencias: <Text style={styles.textDentro}>{existenciasProducto} {(existenciasProducto===1) ?'Unidad':'Unidades'}</Text></Text>
-       <TouchableOpacity style={styles.button} onPress={accionBotonProducto}>
-         <Text style={styles.buttonText}>Seleccionar Producto</Text>
-       </TouchableOpacity>
-       <View style={styles.inputContainer}>
+export default function SelectProduct({ ip, imagenProducto, idProducto, nombreProducto, descripcionProducto
+  , precioProducto, existenciasProducto, accionBotonProducto
+}) {
+  // Definición del componente SelectProduct
+  return (
+    // Contenedor principal de la selección del libro
+    <View style={styles.card}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{ uri: `${ip}/NewPowerLetters/api/images/productos/${imagenProducto}` }}
+          style={styles.image}
+          resizeMode="contain" // Ajustar la imagen al contenedor
+        />
+      </View>
+      <Text style={styles.text}>{idProducto}</Text> 
+      <Text style={styles.textTitle}>{nombreProducto}</Text>
+      <Text style={styles.text}>{descripcionProducto}</Text>
+      <Text style={styles.textTitle}>Precio: <Text style={styles.textDentro}>${precioProducto}</Text></Text>
+      <Text style={styles.textTitle}>Existencias: <Text style={styles.textDentro}>{existenciasProducto} {(existenciasProducto === 1) ? 'Unidad' : 'Unidades'}</Text></Text>
+      <TouchableOpacity style={styles.button} onPress={accionBotonProducto}>
+        <Text style={styles.buttonText}>Seleccionar Producto</Text>
+      </TouchableOpacity>
+      <View style={styles.inputContainer}>
         <Text>Ingresar Cantidad: </Text>
         <TextInput
           style={styles.input}
@@ -33,84 +33,84 @@ export default function SelectProduct({ip, imagenProducto, idProducto, nombrePro
           keyboardType="numeric"
         />
       </View>
-     </View>
+    </View>
 
-    );
+  );
 }
 
-
+//Estilo del componente
 const styles = StyleSheet.create({
-    containerFlat: {
-        flex: 1,
-        marginTop: StatusBar.currentHeight || 0,
-      },
-    container: {
-      flex: 1,
-      backgroundColor: '#f0ecfc',
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: StatusBar.currentHeight || 0,
+  containerFlat: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#f0ecfc',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  card: {
+    backgroundColor: '#ffffff',
+    borderRadius: 8,
+    padding: 16,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    card: {
-        backgroundColor: '#ffffff',
-        borderRadius: 8,
-        padding: 16,
-        marginVertical: 8,
-        marginHorizontal: 16,
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-      },
-      text: {
-        fontSize: 16,
-        marginBottom: 8,
-      },
-      textTitle: {
-        fontSize: 16,
-        marginBottom: 8,fontWeight:'700'
-      },
-      inputContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 8,
-      },
-      input: {
-        flex: 1,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 5,
-        padding: 8,
-        marginLeft: 8,
-      },
-      button: {
-        backgroundColor: '#AF8260',
-        borderRadius: 5,
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        alignItems: 'center',
-      },
-      buttonText: {
-        color: '#ffffff',
-        fontSize: 16,
-        fontWeight:'600'
-      },
-      image: {
-        width: '65%',
-        height: 150,
-        borderRadius: 8,
-        marginBottom: 12,
-      }, 
-      imageContainer: {
-        alignItems: 'center', // Centrar imagen horizontalmente
-      },textDentro:{
-        fontWeight:'400'
-      }
-  });
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  textTitle: {
+    fontSize: 16,
+    marginBottom: 8, fontWeight: '700'
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 8,
+    marginLeft: 8,
+  },
+  button: {
+    backgroundColor: '#AF8260',
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: '600'
+  },
+  image: {
+    width: '65%',
+    height: 150,
+    borderRadius: 8,
+    marginBottom: 12,
+  },
+  imageContainer: {
+    alignItems: 'center', // Centrar imagen horizontalmente
+  }, textDentro: {
+    fontWeight: '400'
+  }
+});
 
 // Realizar una solicitud GET
 fetch('https://jsonplaceholder.typicode.com/posts')
@@ -128,7 +128,7 @@ fetch('https://jsonplaceholder.typicode.com/posts')
   });
 
 
-  // Realizar una solicitud POST
+// Realizar una solicitud POST
 fetch('https://jsonplaceholder.typicode.com/posts', {
   method: 'POST',
   headers: {
@@ -155,14 +155,14 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
 
 
 
-  // Realizar una solicitud GET
+// Realizar una solicitud GET
 axios.get('https://jsonplaceholder.typicode.com/posts')
-.then(response => {
-  console.log(response.data); // Manejo de los datos obtenidos
-})
-.catch(error => {
-  console.error('Error al realizar la solicitud:', error);
-});
+  .then(response => {
+    console.log(response.data); // Manejo de los datos obtenidos
+  })
+  .catch(error => {
+    console.error('Error al realizar la solicitud:', error);
+  });
 
 // Realizar una solicitud POST
 axios.post('https://jsonplaceholder.typicode.com/posts', {
