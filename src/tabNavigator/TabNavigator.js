2e9893e10f7026e7e99eba39289c18133a80dec6
TeresaRuiz <20220651@ricaldone.edu.sv>
2024-07-08 23:47:08 -0600

    dandole estilo al tab Nav

--- src/tabNavigator/TabNavigator.js
+++ src/tabNavigator/TabNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { Platform } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
 
 // Importa tus componentes de pantalla aquí
 import Productos from '../screens/Productos';
@@ -14,57 +14,77 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        headerShown: false, // Oculta el header
-        tabBarActiveTintColor: '#5064d4', // Color de los íconos activos
-        tabBarInactiveTintColor: '#5865E0', // Color de los íconos inactivos
-        tabBarStyle: {
-          backgroundColor: '#FFF',
-          height: Platform.OS === 'ios' ? 80 : 60, // Estilo de la barra de pestañas, altura diferente para iOS y Android
-          borderTopWidth: 0
-        }, // Estilo de la barra de pestañas
-        tabBarIcon: ({ focused, color, size }) => { // Función que define el ícono de la pestaña
-          let iconName;
-          if (route.name === 'Home') {
-            iconName = focused ? 'home' : 'home-outline';
-          } else if (route.name === 'Productos') {
-            iconName = focused ? 'search' : 'search-outline';
-          } else if (route.name === 'Carrito') {
-            iconName = focused ? 'cart' : 'cart-outline';
-          } else if (route.name === 'Libros') {
-            iconName = focused ? 'book' : 'book-outline';
-          }else if (route.name === 'Historial') {
-            iconName = focused ? 'book' : 'book-outline';
-          }
-          return <Ionicons name={iconName} color={color} size={size} />;
-        },
-      })}
-    >
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{ title: 'Inicio' }}
-      />
-      <Tab.Screen
-        name="Libros"
-        component={Libros}
-        options={{ title: 'Libros' }}
-      />
-      <Tab.Screen
-        name="Carrito"
-        component={Carrito}
-        options={{ title: 'Carrito' }}
-      />
-      <Tab.Screen
-        name="Historial"
-        component={Historial}
-        options={{ title: 'Historial' }}
-      />
-    </Tab.Navigator>
+    <View style={styles.container}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false, // Oculta el header
+          tabBarActiveTintColor: '#5064d4', // Color de los íconos activos
+          tabBarInactiveTintColor: '#5865E0', // Color de los íconos inactivos
+          tabBarStyle: {
+            position: 'absolute',
+            bottom: 20, // Eleva la barra de pestañas
+            left: 10,
+            right: 10,
+            elevation: 10, // Sombra en Android
+            backgroundColor: '#f0ecfc',
+            borderRadius: 15, // Bordes redondeados
+            height: Platform.OS === 'ios' ? 80 : 60, // Altura diferente para iOS y Android
+            paddingBottom: Platform.OS === 'ios' ? 20 : 10,
+            borderTopWidth: 0,
+            shadowOpacity: 0.1, // Sombra en iOS
+            shadowOffset: { width: 0, height: -1 },
+            shadowRadius: 10,
+          },
+          tabBarLabelStyle: {
+            fontSize: 14, // Ajusta el tamaño del texto
+          },
+          tabBarIcon: ({ focused, color, size }) => { // Función que define el ícono de la pestaña
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Productos') {
+              iconName = focused ? 'search' : 'search-outline';
+            } else if (route.name === 'Carrito') {
+              iconName = focused ? 'cart' : 'cart-outline';
+            } else if (route.name === 'Libros') {
+              iconName = focused ? 'book' : 'book-outline';
+            } else if (route.name === 'Historial') {
+              iconName = focused ? 'book' : 'book-outline';
+            }
+            return <Ionicons name={iconName} color={color} size={size} />;
+          },
+        })}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{ title: 'Inicio' }}
+        />
+        <Tab.Screen
+          name="Libros"
+          component={Libros}
+          options={{ title: 'Libros' }}
+        />
+        <Tab.Screen
+          name="Carrito"
+          component={Carrito}
+          options={{ title: 'Carrito' }}
+        />
+        <Tab.Screen
+          name="Historial"
+          component={Historial}
+          options={{ title: 'Historial' }}
+        />
+      </Tab.Navigator>
+    </View>
   );
 };
 
-export default TabNavigator;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0ecfc', // Color de fondo para contrastar la barra de pestañas
+  },
+});
 
+export default TabNavigator;
