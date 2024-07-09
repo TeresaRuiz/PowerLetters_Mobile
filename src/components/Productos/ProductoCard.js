5822a97f993212c1c9bbb700180acc5153d004b4
Enrique2442 <107508345+TeresaRuiz@users.noreply.github.com>
2024-07-09 09:25:25 -0600

    comentando codigo

--- src/components/Productos/ProductoCard.js
+++ src/components/Productos/ProductoCard.js
@@ -1,18 +1,20 @@
 import { StyleSheet, View, Image, Text, TouchableOpacity } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
-
+// Definición del componente ProductoCard
 export default function ProductoCard({
-  ip,
-  imagenProducto,
-  idProducto,
-  nombreProducto,
-  descripcionProducto,
-  precioProducto,
-  existenciasProducto,
-  accionBotonProducto,
+  ip, // Dirección IP del servidor para acceder a las imágenes
+  imagenProducto, // Nombre del archivo de imagen del libro
+  idProducto, // ID único del libro
+  nombreProducto, //Título o nombre del libro
+  descripcionProducto, //Descripción del libro
+  precioProducto,  // Precio del libro
+  existenciasProducto, // Cantidad de existencias del libro
+  accionBotonProducto,  // Función a ejecutar al presionar el botón "Añadir al carrito"
 }) {
   return (
+    // Contenedor principal de la tarjeta del libro
     <View style={styles.card}>
+      {/* Contenedor de la imagen del libro */}
       <View style={styles.imageContainer}>
         <Image
           source={{ uri: `${ip}/NewPowerLetters/api/images/productos/${imagenProducto}` }}
@@ -20,24 +22,33 @@ export default function ProductoCard({
           resizeMode="contain"
         />
       </View>
+      {/* Contenedor de la información del libro */}
       <View style={styles.infoContainer}>
+        {/* Mostrar el ID del libro */}
         <Text style={styles.text}>#{idProducto}</Text>
+        {/* Mostrar el título del libro */}
         <Text style={styles.textTitle}>Título: <Text style={styles.textDentro}>{nombreProducto}</Text></Text>
+        {/* Mostrar la descripción del libro */}
         <Text style={styles.textTitle}>Descripción: <Text style={styles.textDentro}>{descripcionProducto}</Text></Text>
+        {/* Mostrar el precio del libro */}
         <Text style={styles.textTitle}>Precio: <Text style={styles.textDentro}>${precioProducto}</Text></Text>
+        {/* Mostrar las existencias del libro */}
         <Text style={styles.textTitle}>Existencias: <Text style={styles.textDentro}>{existenciasProducto} {(existenciasProducto === 1) ? 'Unidad' : 'Unidades'}</Text></Text>
+        {/* Botón "Añadir al carrito" */}
         <TouchableOpacity
           style={styles.cartButton}
           onPress={accionBotonProducto}
         >
+          {/* Icono de círculo más */}
           <FontAwesome name="plus-circle" size={24} color="white" />
+           {/* Texto del botón */}
           <Text style={styles.cartButtonText}>Añadir al carrito</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
 }
-
+// Definición de estilos
 const styles = StyleSheet.create({
   card: {
     backgroundColor: '#ffffff',
