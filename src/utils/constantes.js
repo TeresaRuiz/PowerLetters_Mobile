04db8962750b70813cf23da9b08420b246a2aa31
TeresaRuiz <20220651@ricaldone.edu.sv>
2024-08-18 12:55:27 -0600

    agregando buton

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -2,5 +2,5 @@
 //IP de donde se encuentra la Api, en este caso la dirección 
 //Ip de la maquina local 
 //utilizar la direccion IP del servidor y no localhost
-export const IP = 'http://192.168.203.121';
+export const IP = 'http://192.168.105.202';
 
