723cfa7e8adf1aef22034fa30bd0918027f3472a
TeresaRuiz <20220651@ricaldone.edu.sv>
2024-08-17 18:54:10 -0600

    ya se en los libros desde mi base

---
+++ src/components/Libros/LibroCard.js
@@ -0,0 +1,99 @@
+import { StyleSheet, View, Image, Text, TouchableOpacity } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+export default function LibroCard({
+  ip,
+  imagenLibro,
+  idLibro,
+  tituloLibro,
+  descripcionLibro,
+  precioLibro,
+  accionBotonLibro,
+}) {
+  return (
+    <View style={styles.card}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{ uri: `${ip}/PowerLetters_TeresaVersion/api/images/libros/${imagenLibro}` }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+      </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.text}>#{idLibro}</Text>
+        <Text style={styles.textTitle}>Título: <Text style={styles.textDentro}>{tituloLibro}</Text></Text>
+        <Text style={styles.textTitle}>Descripción: <Text style={styles.textDentro}>{descripcionLibro}</Text></Text>
+        <Text style={styles.textTitle}>Precio: <Text style={styles.textDentro}>${precioLibro}</Text></Text>
+        <TouchableOpacity
+          style={styles.cartButton}
+          onPress={accionBotonLibro}
+        >
+          <FontAwesome name="plus-circle" size={24} color="white" />
+          <Text style={styles.cartButtonText}>Añadir al carrito</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#ffffff',
+    borderRadius: 8,
+    padding: 16,
+    marginVertical: 1,
+    marginHorizontal: 16,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: '40%',
+    alignItems: 'center',
+  },
+  infoContainer: {
+    width: '60%',
+    paddingLeft: 16,
+  },
+  image: {
+    width: '100%',
+    height: 150,
+    borderRadius: 8,
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  textTitle: {
+    fontSize: 16,
+    marginBottom: 8,
+    fontWeight: '700',
+  },
+  textDentro: {
+    fontWeight: '400',
+  },
+  cartButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#5064d4',
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginVertical: 10,
+  },
+  cartButtonText: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: '600',
+    marginLeft: 10,
+    textAlign: 'center',
+  },
+});
--- src/screens/Libros.js
+++ src/screens/Libros.js
@@ -1,69 +1,42 @@
-import { StatusBar, StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, FlatList, SafeAreaView, Image, Modal } from 'react-native';
+import { StatusBar, StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
 import { useState, useEffect } from 'react';
-import * as Constantes from '../utils/constantes'
-import Buttons from '../components/Buttons/Button';
-import ProductoCard from '../components/Productos/ProductoCard';
+import * as Constantes from '../utils/constantes';
+import LibroCard from '../components/Libros/LibroCard'; // Cambia la importación
 import ModalCompra from '../components/Modales/ModalCompra';
-import RNPickerSelect from 'react-native-picker-select';
 import Constants from 'expo-constants';
-import { FontAwesome } from '@expo/vector-icons'; // Importamos el ícono
 
-export default function Productos({ navigation }) {
-
+export default function Libros({ navigation }) {
   const ip = Constantes.IP;
-  const [dataProductos, setDataProductos] = useState([])
-  const [dataCategorias, setDataCategorias] = useState([])
-  const [selectedValue, setSelectedValue] = useState(null);
-  const [cantidad, setCantidad] = useState('');
-  const [modalVisible, setModalVisible] = useState(false)
-  const [idProductoModal, setIdProductoModal] = useState('')
-  const [nombreProductoModal, setNombreProductoModal] = useState('')
-
-  const volverInicio = async () => {
-
-    navigation.navigate('Home');
-
-  };
+  const [dataLibros, setDataLibros] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [idLibroModal, setIdLibroModal] = useState('');
+  const [nombreLibroModal, setNombreLibroModal] = useState('');
 
   const handleCompra = (nombre, id) => {
-    setModalVisible(true)
-    setIdProductoModal(id)
-    setNombreProductoModal(nombre)
-  }
+    setModalVisible(true);
+    setIdLibroModal(id);
+    setNombreLibroModal(nombre);
+  };
 
-  //getCategorias Funcion para consultar por medio de una peticion GET los datos de la tabla categoria que se encuentran en la base de datos
-  const getProductos = async () => {
+  const getLibros = async () => {
     try {
-      const response = await fetch(`${ip}/NewPowerLetters/api/services/public/producto.php?action=readAll`, {
+      const response = await fetch(`${ip}/PowerLetters_TeresaVersion/api/services/public/libros.php?action=readAll`, {
         method: 'GET',
       });
-
       const data = await response.json();
-      console.log("data al obtener productos  \n", data)
       if (data.status) {
-        console.log("trae datos el dataset", data)
-        setDataProductos(data.dataset)
+        setDataLibros(data.dataset);
       } else {
-        console.log("Data en el ELSE error productos", data);
-        // Alert the user about the error
         Alert.alert('Error libros', data.error);
       }
     } catch (error) {
-      console.error(error, "Error desde Catch");
       Alert.alert('Error', 'Ocurrió un error al listar los libros');
     }
-  }
+  };
 
-  //Uso del React Hook UseEffect para que cada vez que se cargue la vista por primera vez
-  //se ejecute la funcion getCategorias
   useEffect(() => {
-    getProductos();
+    getLibros();
   }, []);
-
-  const irCarrito = () => {
-    navigation.navigate('Carrito')
-  }
-
 
   return (
     <View style={styles.container}>
@@ -72,29 +45,26 @@ export default function Productos({ navigation }) {
       <ModalCompra
         visible={modalVisible}
         cerrarModal={setModalVisible}
-        nombreProductoModal={nombreProductoModal}
-        idProductoModal={idProductoModal}
-        cantidad={cantidad}
-        setCantidad={setCantidad}
+        nombreProductoModal={nombreLibroModal}
+        idProductoModal={idLibroModal}
       />
       <SafeAreaView style={styles.containerFlat}>
         <FlatList
-          data={dataProductos}
-          keyExtractor={(item) => item.id_producto}
-          renderItem={({ item }) => ( // Util izamos destructuración para obtener directamente el item
-            <ProductoCard ip={ip}
-              imagenProducto={item.imagen_producto}
-              idProducto={item.id_producto}
-              nombreProducto={item.nombre_producto}
-              descripcionProducto={item.descripcion_producto}
-              precioProducto={item.precio_producto}
-              existenciasProducto={item.existencias_producto}
-              accionBotonProducto={() => handleCompra(item.nombre_producto, item.id_producto)}
+          data={dataLibros}
+          keyExtractor={(item) => item.id_libro} // Ajusta el identificador de libro
+          renderItem={({ item }) => (
+            <LibroCard
+              ip={ip}
+              imagenLibro={item.imagen} // Cambia el nombre a imagenLibro
+              idLibro={item.id_libro} // Cambia el nombre a idLibro
+              tituloLibro={item.titulo_libro} // Cambia el nombre a tituloLibro
+              descripcionLibro={item.descripcion_libro} // Cambia el nombre a descripcionLibro
+              precioLibro={item.precio} // Cambia el nombre a precioLibro
+              accionBotonLibro={() => handleCompra(item.titulo_libro, item.id_libro)}
             />
           )}
         />
       </SafeAreaView>
-
     </View>
   );
 }
@@ -111,106 +81,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
   },
-  card: {
-    backgroundColor: '#ffffff',
-    borderRadius: 8,
-    padding: 16,
-    marginVertical: 1,
-    marginHorizontal: 16,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  text: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  textTitle: {
-    fontSize: 16,
-    marginBottom: 8,
-    fontWeight: '700'
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 8,
-  },
-  input: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 8,
-    marginLeft: 8,
-  },
-  button: {
-    backgroundColor: '#AF8260',
-    borderRadius: 5,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#ffffff',
-    fontSize: 16,
-    fontWeight: '600'
-  },
-  image: {
-    width: '65%',
-    height: 150,
-    borderRadius: 8,
-    marginBottom: 12,
-  },
-  imageContainer: {
-    alignItems: 'center',
-  },
-  textDentro: {
-    fontWeight: '400'
-  },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center',
     marginVertical: 16,
     color: '#5C3D2E',
-  },
-  cartButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#5064d4',
-    borderRadius: 5,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    marginVertical: 10,
-  },
-  cartButtonText: {
-    color: '#ffffff',
-    fontSize: 16,
-    fontWeight: '600',
-    marginLeft: 10,
-  },
-  subtitle: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginVertical: 5,
-    marginHorizontal: 5,
-    color: '#5C3D2E', // Brown color for the title
-  },
-  pickerContainer: {
-    borderWidth: 1,
-    borderColor: '#5064d4', // Color del borde
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    marginBottom: 10,
-    backgroundColor: '#5064d4', // Color de fondo
-  },
-  picker: {
-    color: '#FFFFFF'
   },
 });
