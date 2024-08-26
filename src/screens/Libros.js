d40a889a6f864c7eecbc4657e1d87313b8ba8ff3
TeresaRuiz <20220651@ricaldone.edu.sv>
2024-08-26 15:08:14 -0600

    Buscador

--- src/screens/Libros.js
+++ src/screens/Libros.js
@@ -1,26 +1,28 @@
-import { StatusBar, StyleSheet, Text, View, FlatList, SafeAreaView, Alert } from 'react-native';
+import { StatusBar, StyleSheet, Text, View, FlatList, SafeAreaView, Alert, TextInput } from 'react-native';
 import { useState, useEffect } from 'react';
-import * as Constantes from '../utils/constantes'; // Importa constantes para la configuración de la API
-import LibroCard from '../components/Libros/LibroCard';  // Componente para mostrar información de un libro
-import ModalCompra from '../components/Modales/ModalCompra'; // Modal para realizar la compra
-import Constants from 'expo-constants'; // Importa constantes de Expo
+import * as Constantes from '../utils/constantes'; 
+import LibroCard from '../components/Libros/LibroCard';  
+import ModalCompra from '../components/Modales/ModalCompra'; 
+import Constants from 'expo-constants'; 
 
 export default function Libros({ navigation }) {
   // IP de la API
   const ip = Constantes.IP;
-   // Estados para manejar la lista de libros y el modal de compra
+  // Estados para manejar la lista de libros y el modal de compra
   const [dataLibros, setDataLibros] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
   const [idLibroModal, setIdLibroModal] = useState('');
   const [nombreLibroModal, setNombreLibroModal] = useState('');
-  const [cantidad, setCantidad] = useState(''); // Estado para la cantidad
-// Función para manejar la acción de compra
-  const handleCompra = (nombre, id) => {
-    setModalVisible(true); // Muestra el modal de compra
-    setIdLibroModal(id); // Establece el ID del libro en el modal
-    setNombreLibroModal(nombre); // Establece el nombre del libro en el modal
-  };
-  // Función para obtener la lista de libros desde la API
+  const [cantidad, setCantidad] = useState('');
+  const [searchText, setSearchText] = useState(''); // Estado para el texto de búsqueda
+
+ // Función para manejar la acción de compra
+ const handleCompra = (nombre, id) => {
+  setModalVisible(true); // Muestra el modal de compra
+  setIdLibroModal(id); // Establece el ID del libro en el modal
+  setNombreLibroModal(nombre); // Establece el nombre del libro en el modal
+};
+// Función para obtener la lista de libros desde la API
   const getLibros = async () => {
     try {
         // Realiza una solicitud GET a la API para obtener todos los libros
@@ -28,58 +30,67 @@ export default function Libros({ navigation }) {
         method: 'GET',
       });
       const data = await response.json();
-      // Verifica si la respuesta es exitosa
       if (data.status) {
-        setDataLibros(data.dataset); // Actualiza el estado con la lista de libros
+        setDataLibros(data.dataset);
       } else {
-        Alert.alert('Error libros', data.error);  // Muestra un mensaje de error si no se pueden obtener los libros
+        Alert.alert('Error libros', data.error);
       }
     } catch (error) {
-      Alert.alert('Error', 'Ocurrió un error al listar los libros'); // Maneja cualquier error durante la solicitud
+      Alert.alert('Error', 'Ocurrió un error al listar los libros');
     }
   };
 // useEffect para obtener la lista de libros al montar el componente
   useEffect(() => {
     getLibros();
   }, []);
+
+  // Filtra los libros según el texto de búsqueda
+  const filteredLibros = dataLibros.filter(libro =>
+    libro.titulo_libro.toLowerCase().includes(searchText.toLowerCase())
+  );
 // Renderizado del componente principal
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Libros nuevos</Text>
- {/* Modal para realizar la compra */}
+      
+      {/* Campo de entrada para el buscador */}
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Buscar libros..."
+        value={searchText}
+        onChangeText={setSearchText}
+      />
+
       <ModalCompra
         visible={modalVisible}  // Controla la visibilidad del modal
-        cerrarModal={setModalVisible} // Función para cerrar el modal
-        nombreLibroModal={nombreLibroModal} // Nombre del libro en el modal
-        idLibroModal={idLibroModal} // ID del libro en el modal
-        cantidad={cantidad}  // Cantidad de libros a comprar
-        setCantidad={setCantidad} // Función para actualizar la cantidad
+        cerrarModal={setModalVisible}
+        nombreLibroModal={nombreLibroModal}
+        idLibroModal={idLibroModal}
+        cantidad={cantidad}
+        setCantidad={setCantidad}
       />
       <SafeAreaView style={styles.containerFlat}>
         <FlatList
-          data={dataLibros} // Lista de libros
-          keyExtractor={(item) => item.id_libro.toString()} // Clave única para cada libro
+          data={filteredLibros} // Utiliza la lista filtrada
+          keyExtractor={(item) => item.id_libro.toString()}
           renderItem={({ item }) => (
             <LibroCard
               ip={ip}
-              imagenLibro={item.imagen} // Imagen del libro
-              idLibro={item.id_libro} // ID del libro
-              tituloLibro={item.titulo_libro}  // Título del libro
-              descripcionLibro={item.descripcion_libro}// Descripción del libro
-              precioLibro={item.precio} // Precio del libro
-              navigation={navigation} // Pasa la navegación al componente LibroCard
+              imagenLibro={item.imagen}
+              idLibro={item.id_libro}
+              tituloLibro={item.titulo_libro}
+              descripcionLibro={item.descripcion_libro}
+              precioLibro={item.precio}
+              navigation={navigation}
               accionBotonLibro={() => handleCompra(item.titulo_libro, item.id_libro)}
-              // Maneja la acción de compra
             />
           )}
         />
-
       </SafeAreaView>
-      <Text style={styles.title}></Text>
     </View>
   );
 }
-// Estilos del componente
+//Estilo del componente
 const styles = StyleSheet.create({
   containerFlat: {
     flex: 1,
@@ -98,5 +109,14 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginVertical: 16,
     color: '#5C3D2E',
+  },
+  searchInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+    width: '90%', // Ajusta el ancho según sea necesario
   },
 });
